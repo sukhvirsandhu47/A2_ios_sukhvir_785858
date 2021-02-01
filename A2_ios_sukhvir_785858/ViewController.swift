27e197c8935aa94e93a77570a8203890fe412d3d
Sukhvir <sandhusukhvir20@gmail.com>
2021-02-01 10:40:08 -0500

    Initial Commit

---
+++ A2_ios_sukhvir_785858/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_ios_sukhvir_785858
+//
+//  Created by user185614 on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
